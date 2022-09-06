e2b8fd31c2d52da96c953513404d4a52413d2cfd
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-09-06 16:52:57 -0700

    made users routes and added to index

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const categoriesRouter = require("./categories");
 const productsRouter = require("./products");
+const usersRouter = require("./users");
 
 router.use("/categories", categoriesRouter);
 router.use("/products", productsRouter);
+router.use("/users", usersRouter);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post("/", (req, res) => {
+  res.send(`create a new user`);
+});
+
+router.get("/", (req, res) => {
+  res.send("list of all users");
+});
+
+router.get("/:id", (req, res) => {
+  res.send(`Send user whose id is ${req.params.id}`);
+});
+
+router.put("/:id", (req, res) => {
+  res.send(`update user whose id is ${req.params.id}`);
+});
+
+router.delete("/:id", (req, res) => {
+  res.send(`delete user whose id is ${req.params.id}`);
 });
 
 module.exports = router;
