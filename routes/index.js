11e11f29139e22951269d46f9c1b2981ec82e578
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-09-06 16:50:59 -0700

    made product routes and added to index

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const categoriesRouter = require("./categories.js");
+const categoriesRouter = require("./categories");
+const productsRouter = require("./products");
 
 router.use("/categories", categoriesRouter);
+router.use("/products", productsRouter);
 
 module.exports = router;
---
+++ routes/products.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+
+router.post("/", (req, res) => {
+  res.send(`create a new product`);
+});
+
+router.get("/", (req, res) => {
+  res.send("list of all products");
+});
+
+router.get("/:id", (req, res) => {
+  res.send(`Send product whose id is ${req.params.id}`);
+});
+
+router.put("/:id", (req, res) => {
+  res.send(`update product whose id is ${req.params.id}`);
+});
+
+router.delete("/:id", (req, res) => {
+  res.send(`delete product whose id is ${req.params.id}`);
+});
+
+module.exports = router;
