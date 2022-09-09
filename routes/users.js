15f57369ea6aa8e0f49acef6cae4fdb24f4c704d
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-09-09 15:05:32 -0700

    checked for username and password, sent 400 if not in request

--- routes/users.js
+++ routes/users.js
@@ -2,23 +2,30 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/", (req, res) => {
-  res.send(`create a new user`);
+  const authSubmitted = (req) => {
+    return req.body.username && req.body.password;
+  };
+  if (authSubmitted(req)) {
+    res.json({ message: "create a new user" });
+  } else {
+    res.status(400).json({ message: "Username and password required" });
+  }
 });
 
 router.get("/", (req, res) => {
-  res.send("list of all users");
+  return res.json({ message: "list of all users" });
 });
 
 router.get("/:id", (req, res) => {
-  res.send(`Send user whose id is ${req.params.id}`);
+  res.json({ message: `Send user whose id is ${req.params.id}` });
 });
 
 router.put("/:id", (req, res) => {
-  res.send(`update user whose id is ${req.params.id}`);
+  res.json({ message: `update user whose id is ${req.params.id}` });
 });
 
 router.delete("/:id", (req, res) => {
-  res.send(`delete user whose id is ${req.params.id}`);
+  res.json({ message: `delete user whose id is ${req.params.id}` });
 });
 
 module.exports = router;
