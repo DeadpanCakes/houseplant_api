82d73fb778eedc7582a7656d088d450609c16e3d
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-09-09 16:02:04 -0700

    extracted handlers into category controller object

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,8 +1,22 @@
 const categoryController = () => {
+  const post = (req, res) => {
+    res.send(
+      `create a new category with name: ${req.body.name} and description: ${req.body.description}`
+    );
+  };
   const get = (req, res) => {
     res.json({ message: "list of all categories" });
   };
-  return { get };
+  const getOne = (req, res) => {
+    res.json({ message: `Send category whose id is ${req.params.id}` });
+  };
+  const put = (req, res) => {
+    res.json(`update category whose id is ${req.params.id}`);
+  };
+  const del = (req, res) => {
+    res.send(`delete category whose id is ${req.params.id}`);
+  };
+  return { post, get, getOne, put, del };
 };
 
 module.exports = categoryController();
--- routes/categories.js
+++ routes/categories.js
@@ -2,24 +2,14 @@ const express = require("express");
 const router = express.Router();
 const categoryController = require("../controllers/categories");
 
-router.post("/", (req, res) => {
-  res.send(
-    `create a new category with name: ${req.body.name} and description: ${req.body.description}`
-  );
-});
+router.post("/", categoryController.post);
 
 router.get("/", categoryController.get);
 
-router.get("/:id", (req, res) => {
-  res.send(`Send category whose id is ${req.params.id}`);
-});
+router.get("/:id", categoryController.getOne);
 
-router.put("/:id", (req, res) => {
-  res.send(`update category whose id is ${req.params.id}`);
-});
+router.put("/:id", categoryController.put);
 
-router.delete("/:id", (req, res) => {
-  res.send(`delete category whose id is ${req.params.id}`);
-});
+router.delete("/:id", categoryController.del);
 
 module.exports = router;
