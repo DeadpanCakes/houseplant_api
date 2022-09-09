7c505e8c66459c41bbd0e1c02fabe049b7b904d2
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-09-09 16:21:52 -0700

    added helmet as dependency

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const cors = require("cors");
+const helmet = require("helmet");
 
 var indexRouter = require("./routes/index");
 
@@ -13,6 +14,7 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
+app.use(helmet());
 app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
