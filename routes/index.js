a6989be2154e6e956d336b4c7b2c586b109b63be
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-09-09 15:11:34 -0700

    added login to router index

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,13 @@ const productsRouter = require("./products");
 const usersRouter = require("./users");
 const listsRouter = require("./lists");
 const orderRouter = require("./orders");
+const loginRouter = require("./login");
 
 router.use("/categories", categoriesRouter);
 router.use("/products", productsRouter);
 router.use("/users", usersRouter);
 router.use("/lists", listsRouter);
 router.use("/orders", orderRouter);
+router.use("/login", loginRouter);
 
 module.exports = router;
