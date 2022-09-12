ee80566abc1d0bd4e151e915000787c8cc4711e7
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-09-12 14:01:31 -0700

    made method for posting products

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,26 @@
+const Product = require("../models/Product");
+const Category = require("../models/Category");
+
 const productController = () => {
-  const post = (req, res) => {
-    res.send(
-      `create a new category with name: ${req.body.name} and description: ${req.body.description}`
-    );
+  const post = async (req, res) => {
+    try {
+      const { name, description, price, stock, isPublished } = req.body;
+      const category = await Category.findById(req.body.category).exec();
+      const newProduct = new Product({
+        name,
+        description,
+        price,
+        stock,
+        isPublished,
+        category,
+        discount: 0,
+      });
+      return res.json({ message: "Product Created", data: newProduct });
+    } catch (error) {
+      res.status(500).json({ message: "There Was An Error" });
+    }
   };
+
   const get = (req, res) => {
     res.json({ message: "product of all lists" });
   };
