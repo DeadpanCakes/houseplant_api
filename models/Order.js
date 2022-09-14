ccb05e47cd5258b9eca0c86951268e8783943460
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-09-13 19:16:15 -0700

    reworked orders so that each item is its own model

--- models/Order.js
+++ models/Order.js
@@ -1,8 +1,15 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
+const itemSchema = new Schema({
+  product: { type: mongoose.Types.ObjectId, ref: "Product" },
+  quantity: Number,
+});
+
+const OrderItem = mongoose.model("OrderItem", itemSchema);
+
 const orderSchema = new Schema({
-  items: [{ type: mongoose.Types.ObjectId, ref: "Product" }],
+  items: [{ type: mongoose.Types.ObjectId, ref: "OrderItem" }],
   address: {
     street: String,
     postal: String,
@@ -16,4 +23,6 @@ const orderSchema = new Schema({
   total: Number,
 });
 
-module.exports = mongoose.model("Order", orderSchema);
+const Order = mongoose.model("Order", orderSchema);
+
+module.exports = { Order, OrderItem };
