6ee4e61732be8c43faa9abb4a4f443dcd402d148
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-09-15 15:57:57 -0700

    added basic authentication logic

--- controllers/login.js
+++ controllers/login.js
@@ -1,15 +1,32 @@
+const User = require("../models/User");
+
 const loginController = () => {
-  const post = (req, res) => {
+  const post = async (req, res) => {
     const authSubmitted = (req) => {
-      return req.body.username && req.body.password;
+      return req.body.email && req.body.password;
     };
     if (authSubmitted(req)) {
-      res.json({ message: "create a new user" });
+      const user = await findUser(req.body.email);
+      const isValid = validatePass(user || { password: "" }, req.body.password);
+      if (user && isValid) {
+        return res.json({ message: "logged in" });
+      } else {
+        return res.json({
+          message:
+            "Something went wrong. Check information provided and try again",
+        });
+      }
     } else {
       res.status(400).json({ message: "Username and password required" });
     }
   };
   return { post };
 };
+
+const findUser = async (email) => {
+  return await User.findOne({ email: email }).populate("password");
+};
+
+const validatePass = (user, providedPass) => user.password === providedPass;
 
 module.exports = loginController();
