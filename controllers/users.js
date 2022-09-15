1488da9c0502e78e74e785051a7d1a37874e8577
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-09-14 18:09:25 -0700

    added post method for users

--- controllers/users.js
+++ controllers/users.js
@@ -1,12 +1,16 @@
+const User = require("../models/User");
+
 const userController = () => {
-  const post = (req, res) => {
+  const post = async (req, res) => {
     const authSubmitted = (req) => {
-      return req.body.username && req.body.password;
+      return req.body.email && req.body.password;
     };
     if (authSubmitted(req)) {
-      res.json({ message: "create a new user" });
+      const { email, password } = req.body;
+      const user = await User.create({ email, password });
+      return res.json({ message: "User Created", user });
     } else {
-      res.status(400).json({ message: "Username and password required" });
+      res.status(400).json({ message: "Email and password required" });
     }
   };
   const get = (req, res) => {
